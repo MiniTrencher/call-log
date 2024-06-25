5f5a3857e61e4f9f5ff911ac6d7d963177186b7a
MiniTrencher <manager@minitrencher.com>
2024-06-25 08:03:34 -0700

    Adds: ToggleButton to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import ToggleButton from "./components/ToggleButton";
 
 function App() {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <ToggleButton />
+    </ChakraProvider>
+  );
 }
 
 export default App;
